011487eefedf4cd04d64793732c51ef1adfb2589
gus <ggortiz09@gmail.com>
2020-11-30 15:29:27 -0300

    fix

--- src/main/java/com/gortiz/proyectexample/repository/ProviderRepository.java
+++ src/main/java/com/gortiz/proyectexample/repository/ProviderRepository.java
@@ -1,19 +1,16 @@
 package com.gortiz.proyectexample.repository;
 
-import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
+
+
 import org.springframework.data.jpa.repository.support.JpaRepositoryImplementation;
 import org.springframework.stereotype.Repository;
 
-import com.gortiz.proyectexample.entity.Product;
+
 import com.gortiz.proyectexample.entity.Provider;
 
 @Repository("providerRepository")
 public interface ProviderRepository extends JpaRepositoryImplementation<Provider, Long> {
 	
-	//@Query("select * from product, provider where provider.id=product.fk_provider and product.fk_provider=?1")
-	//@Query("select * from  provider where provider.id=?1")
-	//public List<Provider> fetchByIdWithProviderWhithIdProducto(Long id);
-
+	
 }
